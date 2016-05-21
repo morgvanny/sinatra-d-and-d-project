94d633303aa38bd07d92c9e41dfe36b98706b4bd
morgvanny <morgvanny@gmail.com>
2016-05-21 05:42:25 -0600

    prepping for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,3 @@
-configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
-
-  require 'bundler/setup'
-  Bundler.require(:default, ENV['SINATRA_ENV'])
-
-  ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-  )
-
-  require_all 'app'
-end
-
 configure :production do
    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
